2ffba16163693de6c3092d93ab2f5a97998243e1
Po-Shun <74391244+Po-Shun@users.noreply.github.com>
2021-07-02 15:36:04 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-"# Computer-organization" 
-"# Rush-Hour" 
-"# Rush-Hour" 
-"# Rush-Hour" 
-"# Rush-Hour" 
-"# Rush-Hour" 
-"# Rush-Hour" 
+# Rush Hour solver
+
+In this project, we implement an AI solver for the classic board game "Rush Hour" with A* algorithm.  
+Our goal is to explore better heuristic functions to solve this game.
+
+![](https://github.com/james5418/Rush_Hour_solver/blob/main/game_gui.gif)
+
+### Make sure you have installed these packages before running code
+
+```
+pip install pygame
+```
+
+```
+pip install matplotlib
+```
+
+```
+pip install numpy
+```  
+
+[problem set](https://www.michaelfogleman.com/rush/#DatabaseDownload) we use 
---
+++ analyze/analyze_hard/produce_barchart.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+filename = ["BFS","Manhattan","H2+Manhattan","H3+Manhattan","H4+Manhattan","H5+Manhattan"]
+# text = np.loadtxt("0.txt",dtype = str)
+data = []
+# for i in text[:,0:4]:
+#     data.append((int(i[2]),int(i[3])))
+# data = sorted(data,key = lambda x:x[1], reverse = False)
+# print(data)
+for j in filename:
+    addr = j + ".txt"
+    temp = []
+    text = np.loadtxt(addr,dtype = str)
+    for i in text[:,0:3]:
+        temp.append((int(i[1]),int(i[2])))
+    temp = sorted(temp ,key = lambda x:x[1], reverse = False)
+    data.append(temp)
+totaly = []
+
+for j in range(0,6):
+    y = []
+    for i in range(0,100):
+        pos = data[j][i]
+        y.append(pos[0])
+    totaly.append(y)
+# plt.plot(index,compare)
+avg = []
+for i in totaly:
+    avg.append(np.average(i))
+# plt.savefig('lineChart.png', bbox_inches='tight')
+# plt.show()
+# ax=plt.gca()
+# y_major_locator=MultipleLocator(100)
+# ax.yaxis.set_major_locator(y_major_locator)
+print(avg)
+plt.yscale('log')
+plt.bar(filename,avg,1,color=['crimson','maroon','darkorange','burlywood','gold','tan'])
+plt.axhline(avg[0],ls = "--")
+for a,b in zip(filename,avg):  
+ plt.text(a, b+0.05, '%.0f' % b, ha='center', va= 'bottom',fontsize=11)  
+colors = {"BFS":'crimson',"Manhattan":'maroon',"H2+Manhattan":'darkorange',"H3+Manhattan":'burlywood',"H4+Manhattan":'gold',"H5+Manhattan":'tan'}
+plt.tick_params(
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,         # ticks along the top edge are off
+    labelbottom=False)
+labels = list(colors.keys())
+handles = [plt.Rectangle((0,0),1,1, color=colors[label]) for label in labels]
+plt.legend(handles, labels,loc='right',fontsize = 20)
+plt.xlabel("Heurisitc Function",fontsize=25)
+plt.ylabel("Number Of Expanded Nodes",fontsize=25)
+plt.show()
